8f7dfcad54e0187884ed154a8e63ade46d0f0a71
Corrado-K <kebemartey@gmail.com>
2023-02-28 10:50:03 +0000

    Add calendar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import Search from '../Search/Search';
+import './navbar.css'
+
+const Navbar = () => {
+     return (
+          <div className="navbar-page-header">
+               <div className="navbar-page-search-n-button">
+                    <div className="navbar-page-search">
+                         <Search />
+                    </div>
+
+                    <button>Add Appointment</button>
+               </div>
+          </div>
+     );
+};
+
+export default Navbar;
--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -1,47 +1,41 @@
 import Sidebar from "../../components/Sidebar/Sidebar";
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-
-
-
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import "./calendar.css";
+import Search from "../../components/Search/Search";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Calendar = () => {
-    const events = [
-        { title: 'Meeting', start: new Date() }
-      ]
-
+     const events = [{ title: "Meeting", start: new Date() }];
 
      return (
-          <div className="layout">
-                <div className="layout-content">
-          <div className="home">
-               <div className="middle-content" style={{'background':'white','padding':'20px'}}>
-                    {/* start here */}
-             
-
-                    <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView='dayGridMonth'
-        weekends={false}
-        events={events}
-        eventContent={renderEventContent}
-      />
-                    {/* finis here */}
+          <div className="calendar-page">
+               {/* header */}
+               <div className="calendar-page-header">
+                <Navbar />
                </div>
-             
-          </div>
-          </div>
+
+               {/* calendar */}
+               <div className="calendar">
+                    <FullCalendar
+                         plugins={[dayGridPlugin]}
+                         initialView="dayGridMonth"
+                        //  weekends={false}/
+                         events={events}
+                         eventContent={renderEventContent}
+                    />
+               </div>
           </div>
      );
 };
 
 function renderEventContent(eventInfo) {
-    return (
-      <>
-        <b>{eventInfo.timeText}</b>
-        <i>{eventInfo.event.title}</i>
-      </>
-    )
-  }
+     return (
+          <>
+               <b>{eventInfo.timeText}</b>
+               <i>{eventInfo.event.title}</i>
+          </>
+     );
+}
 
 export default Calendar;

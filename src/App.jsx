64fa00ba0795306d2f3a6e2c201dc8154a27b229
ernestowusu <ernestowusu@ug.edu.gh>
2023-03-01 23:18:55 +0000

    Added Messages route

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,10 @@ function App() {
                          element: <Profile />,
                     },
                     {
+                         path: "messages",
+                         element: <Messages />,
+                    },
+                    {
                          path: "*",
                          element: <PageNotFound />,
                     },

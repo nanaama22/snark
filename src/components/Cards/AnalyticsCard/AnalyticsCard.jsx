ae042da93c98e77712f2f7037fdccb12b866fdd6
otchereErnest <ernestowusu80@gmail.com>
2023-03-29 08:20:20 +0000

    changes

--- src/components/Cards/AnalyticsCard/AnalyticsCard.jsx
+++ src/components/Cards/AnalyticsCard/AnalyticsCard.jsx
@@ -6,7 +6,9 @@ export const AnalyticsCard = () => {
                <div className="analytics">
                     <h5 className="a-label">Successful Consultations</h5>
                     <div className="analytics-graph-1">
-                    <div className="inner"><span className="inner-texxt">1,564</span></div>
+                    <div className="inner">
+                         {/* <span className="inner-texxt">1,564</span> */}
+                         </div>
                     </div>
                     <p className="analytics-desc">
                          Total number of calls, video chats and schedules
@@ -16,7 +18,9 @@ export const AnalyticsCard = () => {
                <div className="analytics">
                     <h5 className="a-label">Total of patients</h5>
                     <div className="analytics-graph-2">
-                    <div className="inner"><span className="inner-textx-1">001</span></div>
+                    <div className="inner">
+                         {/* <span className="inner-textx-1">001</span> */}
+                         </div>
                     </div>
                     <p className="analytics-desc">
                          Total number of patients, you have consulted with
@@ -25,7 +29,9 @@ export const AnalyticsCard = () => {
                <div className="analytics">
                     <h5 className="a-label">Average Daily Hours</h5>
                     <div className="analytics-graph-3">
-                    <div className="inner"><span className="inner-textxx-2">350</span></div>
+                    <div className="inner">
+                         {/* <span className="inner-textxx-2">350</span> */}
+                         </div>
                     </div>
                     <p className="analytics-desc">
                          Average amount of time spent with patients daily
